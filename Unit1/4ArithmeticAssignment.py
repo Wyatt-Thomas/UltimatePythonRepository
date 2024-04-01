327d251039f483e599c0fe05593730d03b77f659
Wyatt Thomas <26wyattthomasusd266.com>
2024-04-01 12:47:26 -0500

    Complet

--- Unit1/4ArithmeticAssignment.py
+++ Unit1/4ArithmeticAssignment.py
@@ -1,23 +1,59 @@
-print("########## 1.4.1 ##########")
+# print("########## 1.4.1 ##########")
+# Number= int(input("please type in a number:"))
+# print(str(Number)+"*5 is",Number*5)
+# print("########## 1.4.2 ##########")
+# Name=input("What is your name?")
+# Year=int(input("Which year were you born"))
+# print("hi",Name,"you will be",2024-Year,"years old at the end of the year 2024")
+# print("########## 1.4.3 ##########")
+# days=int(input("How many days?"))
+# print("seconds in that many days:",days*86400)
+# print("########## 1.4.4 ##########")
+# # Fix the code
+# sum=1
+# number = int(input("Please type in the first number: "))
+# sum*=number
+# number = int(input("Please type in the second number: "))
+# sum*=number
+# number = int(input("Please type in the third number: "))
+# sum*=number
+# print("your prduct is",sum)
 
-print("########## 1.4.2 ##########")
+# print("########## 1.4.5 ##########")
+# sum=0
+# product=1
+# number = int(input("Number 1: "))
+# sum+=number
+# product*=number
+# number = int(input("Number 2: "))
+# sum+=number
+# product*=number
+# print("your sum is",sum)
+# print("your prduct is",product)
+# print("########## 1.4.6 ##########")
+# sum=0
+# number = int(input("Number 1: "))
+# sum+=number
+# number = int(input("Number 2: "))
+# sum+=number
+# number = int(input("Number 3: "))
+# sum+=number
+# number = int(input("Number 4: "))
+# sum+=number
+# print("The sum of the numbers is",str(sum)+"and the Mean is",str(sum/4))
 
-print("########## 1.4.3 ##########")
-
-print("########## 1.4.4 ##########")
-# Fix the code
-number = int(input("Please type in the first number: "))
-number = int(input("Please type in the second number: "))
-number = int(input("Please type in the third number: "))
-
-product = number * number * number
-
-print("The product is", product)
-
-print("########## 1.4.5 ##########")
-
-print("########## 1.4.6 ##########")
-
-print("########## 1.4.7 ##########")
+# print("########## 1.4.7 ##########")
+# sum=0
+# number_eat=int(input("How many times do you eat at the student cafeteria"))
+# student_price=int(input("The price of a typical student lunch"))
+# groceries_price=int(input("how much money do you spend on groceries in a week?"))
+# sum=number_eat*student_price+groceries_price
+# Daily=sum/7
+# print("Average food expenditure:")
+# print("Daily:",Daily)
+# print("Weekly:",sum)
 
 print("########## 1.4.8 ##########")
+students_n=int(input("How many students on the course?"))
+group_size=int(input("Desired group size?"))
+print("Number of groups formed:",students_n//group_size)
