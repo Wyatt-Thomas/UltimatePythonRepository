427570126c586b5c0fc50b46d14bb1467e6cb50d
Wyatt Thomas <26wyattthomasusd266.com>
2024-04-17 11:48:47 -0500

    done with 3.1

--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,21 +1,47 @@
-# ========== 3.1.1 ==========
+# # ========== 3.1.1 ==========
+# number=2
+# while number<32:
+#     print(number)
+#     number+=2
+# print("Now!")
 
-
-# ========== 3.1.2 ==========
+# # ========== 3.1.2 ==========
 
 # print("Are you ready?")
 # number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
+# while number>0:
+#     print(number)
+#     number-=1
 # print("Go!")
 
 # ========== 3.1.3 ==========
-
+# limit=int(input("Upper limit"))
+# number=0
+# while number<limit:
+#     number+=1
+#     print(number)
 
 # ========== 3.1.4 ==========
-
-
+# limit=int(input("Upper limit"))
+# number=1
+# while number<limit:
+#     print(number)
+#     number*=2
+# if number/limit==1:
+#     print(limit)
 # ========== 3.1.5 ==========
-
+# limit=int(input("Upper limit"))
+# base=int(input("Base:"))
+# number=1
+# while number<limit:
+#     print(number)
+#     number*=base
+# if number/limit==1:
+#     print(limit)
 
 # ========== 3.1.6 ==========
+# limit=int(input("Upper limit"))
+# number=0
+# while number<limit:
+#     number=number+1
+# print(number)
